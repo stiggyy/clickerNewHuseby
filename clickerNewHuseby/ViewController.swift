64f3a5c34f5335cbf4ee9da9e8999b1ed86c8f13
c huseby <catherine_huseby@outlook.com>
2023-10-03 09:27:11 -0500

    Initial Commit

---
+++ clickerNewHuseby/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  clickerNewHuseby
+//
+//  Created by CATHERINE HUSEBY on 10/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
