9d68ddec0e7d45ad8abd3bae99f907c0178e7f55
Catherine Huseby <catherine_huseby@outlook.com>
2023-10-03 18:47:56 -0500

    hooray

--- clickerNewHuseby/ViewController.swift
+++ clickerNewHuseby/ViewController.swift
@@ -9,7 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+
     var points: Int = 0
+    
+    var hasCupcake: Bool = false
+    var hasCider: Bool = false
+    var hasFrog: Bool = false
+    
+    var hasBoost: Bool = false
+    var boostAmt: Int = 1
+    
+    var cupcakeAmt: Int = 0
+    var ciderAmt: Int = 0
+    var frogAmt: Int = 0
+    
+    
+    @IBOutlet weak var boostLabelOutlet: UILabel!
+    
+    @IBOutlet weak var cupcakeImageOutlet: UIImageView!
+    @IBOutlet weak var ciderImageOutlet: UIImageView!
+    @IBOutlet weak var frogImageOutlet: UIImageView!
+    
+    @IBOutlet weak var cupcakeLabelOutlet: UILabel!
+    @IBOutlet weak var ciderLabelOutlet: UILabel!
+    @IBOutlet weak var frogLabelOutlet: UILabel!
     
     @IBOutlet weak var pointOutlet: UILabel!
     
@@ -18,9 +41,28 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        pointOutlet.text = "Points: \(points)"
+        if hasCupcake {
+            cupcakeImageOutlet.image = UIImage(named: "cake")
+            cupcakeLabelOutlet.text = "cupcakes: \(cupcakeAmt)"
+        }
+        if hasCider { ciderImageOutlet.image = UIImage(named: "cider")
+            ciderLabelOutlet.text = "ciders: \(ciderAmt)"
+        }
+        if hasFrog {
+            frogImageOutlet.image = UIImage(named: "frog")
+            frogLabelOutlet.text = "frogs: \(frogAmt)"
+        }
+        if hasBoost {
+            boostLabelOutlet.text = "(\(boostAmt)x boost)"
+        }
+    }
+    
     @IBAction func addPoints(_ sender: Any) {
         
-        points = points + 1
+        points = points + boostAmt
+        
         pointOutlet.text = "Points: \(points)"
     }
     
@@ -36,7 +78,6 @@ class ViewController: UIViewController {
         let nvc = segue.destination as! ViewControllerStore
         
         nvc.points = points
-        
         
         //making delgate a reference
        nvc.delegate = self
--- clickerNewHuseby/ViewControllerStore.swift
+++ clickerNewHuseby/ViewControllerStore.swift
@@ -12,7 +12,11 @@ class ViewControllerStore: UIViewController {
     @IBOutlet weak var pointsOutlet: UILabel!
     var points: Int = 0
     var delegate: ViewController!
+    
+    @IBOutlet weak var boostLabelOutlet: UILabel!
+    
 
+    @IBOutlet weak var errorOutlet: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,17 +25,82 @@ class ViewControllerStore: UIViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         pointsOutlet.text = "Points: \(points)"
+        boostLabelOutlet.text = "\(delegate.boostAmt + 1)x points: 25 points"
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func buyCupcake(_ sender: Any) {
+        
+        if points >= 5 {
+            
+            points = points - 5
+            delegate.points = points
+            pointsOutlet.text = "Points: \(points)"
+            delegate.hasCupcake = true
+            delegate.cupcakeAmt = delegate.cupcakeAmt + 1
+            
+        } else {
+            errorOutlet.text = "Not enough points, try again!"
+            
+        }
     }
-    */
+    
+    @IBAction func buyCider(_ sender: Any) {
+        
+        if points >= 10 {
+            
+            points = points - 10
+            delegate.points = points
+            pointsOutlet.text = "Points: \(points)"
+            delegate.hasCider = true
+            delegate.ciderAmt = delegate.ciderAmt + 1
+            
+        } else {
+            errorOutlet.text = "Not enough points, try again!"
+            
+        }
 
+    }
+    
+    @IBAction func buyFrog(_ sender: Any) {
+        
+        if points >= 15 {
+            
+            points = points - 15
+            delegate.points = points
+            pointsOutlet.text = "Points: \(points)"
+            delegate.hasFrog = true
+            delegate.frogAmt = delegate.frogAmt + 1
+            
+        } else {
+            errorOutlet.text = "Not enough points, try again!"
+            
+        }
+
+        
+        
+    }
+    
+    
+    @IBAction func boostBuy(_ sender: Any) {
+        
+        if points >= 25 {
+            
+            points = points - 25
+            delegate.points = points
+            pointsOutlet.text = "Points: \(points)"
+            delegate.hasBoost = true
+            delegate.boostAmt = delegate.boostAmt + 1
+            boostLabelOutlet.text = "\(delegate.boostAmt + 1)x points: 25 points"
+            
+        } else {
+            errorOutlet.text = "Not enough points, try again!"
+            
+        }
+        
+    }
+    
+    
+    
+    
 }
